f3785d94d357e744db61b48d3759421fde6cad57
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-01-04 21:42:23 -0500

    * add child constructor test.

--- test/clustered-node/childTest.js
+++ test/clustered-node/childTest.js
@@ -2,11 +2,20 @@
  * Created by Rana on 15-01-04.
  */
 var expect   = require("chai").expect;
+var http     = require("http");
+
+var Child = require("../../lib/clustered-node/child");
 
 describe("child", function(){
     describe("construct", function() {
-        it("create child", function () {
-            expect(true).eql(true);
+        it("create child fail", function () {
+            var worker = new Child();
+            expect(worker).instanceOf(Error);
+        });
+        it("create child success", function(){
+            var server = http.createServer();
+            var worker = new Child({port:1234}, server);
+            expect(worker).instanceOf(Child);
         });
     });
 });
